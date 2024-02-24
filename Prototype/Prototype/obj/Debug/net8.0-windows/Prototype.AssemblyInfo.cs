30649aea26ee60cdfa5ff3f594a604fd9f6b383a
Ahmed al Lakani <ahmed.al.lakani@gmail.com>
2024-02-24 14:09:08 +0200

    aaa

--- Prototype/Prototype/obj/Debug/net8.0-windows/Prototype.AssemblyInfo.cs
+++ Prototype/Prototype/obj/Debug/net8.0-windows/Prototype.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+842d0ecbc13137a8cfe98cb875b943ba9fb2dee3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c227ada7792240d3737508a0ca5943168e82799")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
